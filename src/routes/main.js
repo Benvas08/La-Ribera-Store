6e47bd152aebc409e295d70be795aa4ec2cbe4f5
Benvas08 <benjaminevasques@gmail.com>
2025-06-10 14:16:58 -0300

    Agregue el ruteo de /Registrarse

--- src/routes/main.js
+++ src/routes/main.js
@@ -7,6 +7,7 @@ const mainController = require('./../controller/mainController');
 
 router.get('/', mainController.home);
 router.get('/productos', mainController.productos);
+router.get('/registrarse', mainController.registrarse);
 //router.ger('/contact', mainController.contact);
 
 module.exports = router;
