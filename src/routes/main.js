35c6133980e416c4b247a211b147d820780a3e91
Benvas08 <benjaminevasques@gmail.com>
2025-09-23 14:14:32 -0300

    Se agrego el login con su respectivo funcionamiento. Primero uno se registra y luego se concuerdan los datos obtenido y se inicia sesión. Se agrego un estilo distinto para el login

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,16 +6,19 @@ const mainController = require('./../controller/mainController');
 router.get('/', mainController.home); // Ruta principal que muestra la página de inicio y los servicios
 
 router.get('/servicios', mainController.servicios); // Ruta para mostrar los servicios
+
 // Ruta  para el formulario
-
 router.get('/registrarse', mainController.form);
-
 router.post('/form', mainController.processform);
 
 // Ruta para mostrar las personas registradas
-router.get('/personas', mainController.personas);
-
+router.get('/productos', mainController.personas);
 router.get('/usuarios', mainController.usuarios);
+
+// Ruta para mostrar el login
+router.get('/login', mainController.login);
+router.post('/login', mainController.loginProcess);
+
 // const infoController = require('../controller/infoController');
 
 // router.get('/cafetera', 'views/infoController/cafetera.ejs'); 
